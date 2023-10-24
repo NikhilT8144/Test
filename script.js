4fd7bea05fa16d1b8d6a0f46ff3adf34690a6a44
Nikhil Tiwari <87422608+NikhilT8144@users.noreply.github.com>
2023-10-24 16:47:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ let log = document.getElementById("log");
 let home = document.getElementById("home");
 let time = Date();
 let dt = document.getElementById("dt");
-setInterval(function setdt(){
-  dt.innerHTML = time;
-}, 0);
+
 log.style.display = "block";
 home.style.display = "none";
 function signin(){
   log.style.display = "none";
   home.style.display = "block";
   console.log("Sign In Sucessful on " + time);
+  setInterval(function setdt(){
+  dt.innerHTML = time;
+  }, 0);
   cookies();
 }
 //cookies function
